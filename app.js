efe598aa10bd5f942f7265673693ed0fd702f182
xstephenhu98 <xstephenhu98@gmail.com>
2015-06-09 14:58:38 -0400

    June 9, 2015

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by xstephenhu98 on 06/09/2015.
+ */
+(function(){
+    'use strict';
+
+    angular.module('TNL',['ngMaterial'])
+        .controller('AppCtrl', ['$scope', function($scope){
+
+        }]);
+
+})();
